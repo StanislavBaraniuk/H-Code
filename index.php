3a3fb755a97c6e949941129e77e1a7b0b4a6753a
StanislavBaranuk <stanislavbaraniuk@gmail.com>
2018-06-24 21:28:10 +0300

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include 'node_modules/react-scripts/scripts/start.js';
-include 'node_modules/react-scripts/scripts/build.js';
-include  'node_modules/react-scripts/scripts/test.js';
-include 'node_modules/react-scripts/scripts/eject.js';
-
-include 'public/index.html';
-
